9496271efd7fe67a14467535ec009509e9f38e65
James Rampling <30095774@tafe.wa.edu.au>
2025-03-31 15:59:38 +0800

    Implement data processing class
    Closes #3

---
+++ src/Model/DataProcessing.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace S4UDashboard.Model;
+
+public class DataProcessing
+{
+    private DataProcessing() { }
+    public readonly static DataProcessing Instance = new();
+
+    /// <summary>
+    /// Given a <c>SensorDataModel</c> constructs and returns a <c>CalculatedDataModel</c> based
+    /// on the samples contained in <c>sensorData</c>.
+    /// </summary>
+    public CalculatedDataModel CalculateAuxilliaryData(SensorDataModel sensorData)
+    {
+        double min = double.PositiveInfinity, max = double.NegativeInfinity, sum = 0.0;
+
+        foreach (var sample in sensorData.Samples.SelectMany(x => x))
+        {
+            if (sample < min) min = sample;
+            if (sample > max) max = sample;
+            sum += sample;
+        }
+
+        int len = sensorData.SensorNames.Length * sensorData.SampleTimes.Length;
+        return new CalculatedDataModel
+        {
+            Mean = sum / len,
+            Minimum = min,
+            Maximum = max,
+        };
+    }
+
+    /// <summary>
+    /// Performs a binary search on <c>sorted</c>, comparing <c>needle</c> to each element
+    /// after having been mapped by <c>selector</c>.
+    /// </summary>
+    public int FindByInSorted<T, S>(IList<T> sorted, Func<T, S> selector, S needle) where S : IComparable<S>
+    {
+        int lowerBound = 0, upperBound = sorted.Count;
+
+        while (lowerBound < upperBound)
+        {
+            var middleIdx = (lowerBound + upperBound) / 2;
+            var current = selector.Invoke(sorted[middleIdx]);
+
+            switch (needle.CompareTo(current))
+            {
+                case 0:
+                    return middleIdx;
+                case var c when c < 0:
+                    upperBound = middleIdx;
+                    break;
+                case var c when c > 0:
+                    lowerBound = middleIdx + 1;
+                    break;
+            }
+        }
+
+        return -1;
+    }
+}
