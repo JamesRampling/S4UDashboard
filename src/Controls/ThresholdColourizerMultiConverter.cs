98a46f55b006ad9e7320e9a7ef467caf2fcb30d6
James Rampling <30095774@tafe.wa.edu.au>
2025-04-07 18:21:16 +0800

    Add cell threshold visualisation
    Closes #7

---
+++ src/Controls/ThresholdColourizerMultiConverter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Avalonia.Media.Immutable;
+
+namespace S4UDashboard.Controls;
+
+public class ThresholdColourizerMultiConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values?.Count != 4 || !targetType.IsAssignableFrom(typeof(ImmutableSolidColorBrush)))
+            throw new NotSupportedException();
+
+        if (values[0] is not string cell ||
+            (values[1] is not double && values[1] is not null) ||
+            (values[2] is not double && values[2] is not null) ||
+            values[3] is not bool vf)
+            throw new NotSupportedException();
+
+        if (!double.TryParse(cell, out var value)) throw new NotSupportedException();
+
+        if (!vf) return Brushes.Transparent;
+        if (values[1] is double lower && value <= lower) return Brushes.Blue;
+        if (values[2] is double upper && value >= upper) return Brushes.Red;
+        return Brushes.Green;
+    }
+}
